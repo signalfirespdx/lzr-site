f45a19bd2058b122962883580f690d4f1516618d
signalfirespdx <signalfirespdx@protonmail.com>
2026-04-11 20:06:42 +0000

    Deploy from signalfirespdx/lzr-src@bd4fd9a750635ef04ec8a33125f2f03d52d9922e

---
+++ _build/assets/2026-02-27-azn-zine-fest-Bg4Ui9K_.js
@@ -0,0 +1,9 @@
+import{M as i}from"./index-PgV2X1eG.js";import{b as t,k as s}from"./web-NfXTu61L.js";const a={name:"Azn Zine Fest",category:"Zine Fest",tags:[],start:"2026-02-28T12:00:00.000-08:00",end:"2026-02-28T17:00:00.000-08:00",repeats:"none",rrule:"",repeat_until:"",location:"Fubonn Shopping Center",address:"2850 SE 82nd Ave, Portland, OR 97266",link:"https://aznzinefest.tumblr.com/",image:"/signal-2026-02-27-140505_002.png",description:`**who**
+
+- all are welcome & free to attend! (masks provided, required, seen as an act of care <3)
+- [over 100 azn zinesters](https://aznzinefest.tumblr.com/vendors) are coming to inspire you with their art and zines\\~
+- organized by local pdx azn folk: Envy Estacio, Yoko O.K., A’misa Chiu, Sarah Joy Calpo, Julia Kim, Natalie Soutonglang, & SJ Anderson
+
+**Special Thanks**
+
+azn zine fest is honored and grateful to be one of 13 amazing projects that was selected to be a 2026 Precipice Fund grant recipient through [PICA PDX](https://www.pica.org/precipice-fund) \\~ it’s such a surreal thing to be able to represent azn creatives in Portland thru this DIY project, and for zines in contemporary art spaces! <3`,featured:!1};function o(e){return[]}function p(e={}){const{wrapper:n}={...i(),...e.components};return n?t(n,s(e,{get children(){return t(o,e)}})):o()}const r={frontmatter:typeof a<"u"?a??{}:{},toc:void 0,editLink:"",lastUpdated:1775937395e3};typeof window<"u"&&(window.$$SolidBase_page_data??={},window.$$SolidBase_page_data["/home/runner/work/lzr-src/lzr-src/content/events/2026-02-27-azn-zine-fest.md"]=r);const l=r;export{l as $$SolidBase_page_data,p as default,a as frontmatter};
