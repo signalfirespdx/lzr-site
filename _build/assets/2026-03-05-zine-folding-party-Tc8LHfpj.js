8e7db7f9b4f0d06a7f0a506efde82a1fa850aeba
signalfirespdx <signalfirespdx@protonmail.com>
2026-05-01 18:46:25 +0000

    Deploy from signalfirespdx/lzr-src@c1d92166fc2d42e1ff9a25250e374a403250ef90

---
+++ _build/assets/2026-03-05-zine-folding-party-Tc8LHfpj.js
@@ -0,0 +1,13 @@
+import{M as i}from"./index-PgV2X1eG.js";import{b as t,k as s}from"./web-NfXTu61L.js";const o={name:"Zine Folding Party!",category:"",tags:[],start:"2026-03-21T13:30:00.000-07:00",end:"2026-03-21T15:30:00.000-07:00",repeats:"none",rrule:"",repeat_until:"",location:"Westside Queer Resource Center",address:"233 SE Washington St Suite 104, Hillsboro, OR 97123",link:"",image:"/signal-2026-03-05-15-46-31-585.jpg",description:`Zine Folding Party with Little Zine Revolution!
+
+Sat. March 21. 1:30PM - 3:30PM
+
+Westside Queer Resource Center
+
+233 SE Washington St Hillsboro, OR
+
+Assemble Anti-colonial zines, pick up Ice-Alert whistles
+
+No experience necessary
+
+Mask encouraged`,featured:!1};function r(e){return[]}function c(e={}){const{wrapper:n}={...i(),...e.components};return n?t(n,s(e,{get children(){return t(r,e)}})):r()}const a={frontmatter:typeof o<"u"?o??{}:{},toc:void 0,editLink:"",lastUpdated:1777660585e3};typeof window<"u"&&(window.$$SolidBase_page_data??={},window.$$SolidBase_page_data["/home/runner/work/lzr-src/lzr-src/content/events/2026-03-05-zine-folding-party.md"]=a);const u=a;export{u as $$SolidBase_page_data,c as default,o as frontmatter};
