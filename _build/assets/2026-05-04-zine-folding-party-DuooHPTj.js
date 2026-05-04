7c6074cd5641d1d2d433aa2277295e9921971c76
signalfirespdx <signalfirespdx@protonmail.com>
2026-05-04 02:42:22 +0000

    Deploy from signalfirespdx/lzr-src@4b5debf83eb8cef9556a33114fc8e00c406ba4eb

---
+++ _build/assets/2026-05-04-zine-folding-party-DuooHPTj.js
@@ -0,0 +1,7 @@
+import{M as i}from"./index-PgV2X1eG.js";import{b as t,k as s}from"./web-NfXTu61L.js";const a={name:"Zine Folding Party!",category:"",tags:[],start:"2026-05-15T18:30:00.000-07:00",end:"2026-05-15T20:00:00.000-07:00",repeats:"none",rrule:"",repeat_until:"",location:"Always Here Bookstore",address:"4555 N. Williams Ave Portland OR",link:"",image:"/event-flyer-ig-2.jpg",description:`Zine Folding Party with Little Zine Revolution!
+
+Assemble queer, anti-fascist, anti-colonial zines
+
+No experience necessary
+
+Masks required`,featured:!1};function o(e){return[]}function c(e={}){const{wrapper:n}={...i(),...e.components};return n?t(n,s(e,{get children(){return t(o,e)}})):o()}const r={frontmatter:typeof a<"u"?a??{}:{},toc:void 0,editLink:"",lastUpdated:1777861985e3};typeof window<"u"&&(window.$$SolidBase_page_data??={},window.$$SolidBase_page_data["/home/runner/work/lzr-src/lzr-src/content/events/2026-05-04-zine-folding-party.md"]=r);const f=r;export{f as $$SolidBase_page_data,c as default,a as frontmatter};
