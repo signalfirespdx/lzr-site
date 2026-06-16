193651de4f7ddb66085235a6103f94881d88c11a
signalfirespdx <signalfirespdx@protonmail.com>
2026-06-16 07:00:14 +0000

    Deploy from signalfirespdx/lzr-src@7da1bc0bde14ab7858c046d09d5308bc674238f0

---
+++ _build/assets/2026-05-28-zine-folding-party-DvzBim-n.js
@@ -0,0 +1,9 @@
+import{M as i}from"./index-D9e6rPla.js";import{g as t,z as d}from"./web-CJvYBidc.js";const r={name:"Zine Folding Party!",category:"",tags:[],start:"2026-06-13T14:00:00.000-07:00",end:"2026-06-13T17:00:00.000-07:00",repeats:"none",rrule:"",repeat_until:"",location:"Third Eye Books Zen Garden",address:"2518 SE 33rd Ave. Portland OR",link:"",image:"/event-flyer-ig-2.png",description:`Celebrate Portland Book Week with LZR (Little Zine Revolution)!
+
+Assemble anti-colonial zines.  No experience necessary.
+
+Check out OUR LFDL reference library!
+
+FREE zines & ICE-alert whistles
+
+All are welcome!`,featured:!1};function o(e){return[]}function c(e={}){const{wrapper:n}={...i(),...e.components};return n?t(n,d(e,{get children(){return t(o,e)}})):o()}const a={frontmatter:typeof r<"u"?r??{}:{},toc:void 0,editLink:"",lastUpdated:1781592614e3};typeof window<"u"&&(window.$$SolidBase_page_data??={},window.$$SolidBase_page_data["/home/runner/work/lzr-src/lzr-src/content/events/2026-05-28-zine-folding-party.md"]=a);const f=a;export{f as $$SolidBase_page_data,c as default,r as frontmatter};
